5b4c0eccc20528851a496bb3a4dee49456dc2433
Allan Arce <aarcef1@miumg.edu.gt>
2026-08-02 20:00:40 -0600

    Ejercicio 1 done

--- Nivel 1/Ejercicio1.java
+++ Nivel 1/Ejercicio1.java
@@ -1,5 +1,18 @@
 public class Ejercicio1 {
+    
     public static void main(String[] args) {
-        
+        try 
+        {
+            int division = 10/0;
+            System.out.println("Intentado pasar: "+ division);
+        } catch (ArithmeticException e) {
+
+            // TODO: handle exception
+            System.out.println("No se puede dividir dentro de 0.");
+        } catch(Exception t)
+        {
+            System.out.println("Error General");
+        } 
+
     }
 }
